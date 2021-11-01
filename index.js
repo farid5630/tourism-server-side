78ee2c8e0cc387c5a7cdafc8dca33c6a60948aad
farid5630 <mdforidulislam5630@gmail.com>
2021-11-02 00:05:54 +0600

    finally commit

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = "mongodb+srv://travelassignment:6LMtxpT8abFxzZWx@cluster0.xdgiu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xdgiu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
